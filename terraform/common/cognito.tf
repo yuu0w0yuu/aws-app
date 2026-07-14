8a74052420e65e59dd9a86a458a21713ba18e9ce
Yutaro Shirayama <yuu0w0yuu@gmail.com>
2026-07-14 19:37:58 +0900

    cognitoの設定を変更した

--- terraform/common/cognito.tf
+++ terraform/common/cognito.tf
@@ -1,14 +1,12 @@
 resource "aws_cognito_user_pool" "main" {
   name = "${local.service}-${local.env}-user-pool"
+
   auto_verified_attributes = ["email"]
   username_attributes      = ["email"]
   mfa_configuration        = "OFF"
+
   password_policy {
     minimum_length    = 8
-    require_lowercase = true
-    require_numbers   = true
-    require_symbols   = true
-    require_uppercase = true
   }
   tags = local.default_tags
 }
